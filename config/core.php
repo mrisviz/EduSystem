d302843e42762c8a604ffa6491f06e6a2f63b3db
Evgeniy Pohomivich <Pohomovi4@mail.ru>
2019-01-22 11:55:18 +0300

    first add sites

---
+++ config/core.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: IT-Admin
+ * Date: 08.10.2018
+ * Time: 17:09
+ */
+function debug($text){
+    echo '<pre>';
+    print_r($text);
+    echo '</pre>';
+}
