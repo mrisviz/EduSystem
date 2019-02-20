891ca14a967902acbaa4a8a89350b05d93571e37
mrisviz <Pohomovi4@mail.ru>
2019-02-20 18:36:10 +0300

    dead line start

--- views/site/login.php
+++ views/site/login.php
@@ -11,18 +11,21 @@ $this->title = 'Вход';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-login col-lg-4 col-lg-offset-4 col-sm-12">
-    <h1 class="page-title"><?= Html::encode($this->title) ?></h1>
+
 
 
     <?php $form = ActiveForm::begin([
         'id' => 'login-form',
         'layout' => 'horizontal',
+        'options' => [
+            'class' => 'ui-form-r'
+        ],
         'fieldConfig' => [
             'template' => "{label}\n<div class=\"col-lg-12\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
             'labelOptions' => ['class' => 'col-lg-3 control-label'],
         ],
     ]); ?>
-
+        <h3 class="page-title"><?= Html::encode($this->title) ?></h3>
         <?= $form->field($model, 'mail')->input('mail',['id'=>'yii']) ?>
 
         <?= $form->field($model, 'password')->passwordInput() ?>
@@ -31,7 +34,7 @@ $this->params['breadcrumbs'][] = $this->title;
             'template' => "<div class=\"col-lg-offset-1 col-lg-3\">{input} {label}</div>\n<div class=\"col-lg-8\">{error}</div>",
         ]) ?>
 
-        <div class="form-group">
+        <div class="form-row">
             <div class="col-lg-offset-1 col-lg-11">
                 <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
             </div>
@@ -40,7 +43,5 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php ActiveForm::end(); ?>
 
     <div class="col-lg-offset-1" style="color:#999;">
-        You may login with <strong>admin/admin</strong> or <strong>demo/demo</strong>.<br>
-        To modify the username/password, please check out the code <code>app\models\User::$users</code>.
     </div>
 </div>
